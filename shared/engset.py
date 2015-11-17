cf4b134de4b04f23eab4fd9a12ad9a41429ade37
josep <josepm.ferrandiz@gmail.com>
2015-11-16 20:27:01 -0800

    some fixes

--- shared/engset.py
+++ shared/engset.py
@@ -1,5 +1,16 @@
 import numpy as np
 import sys
+import os
+import json
+
+# add the path to utilities
+f = os.path.dirname(os.path.realpath(__file__))
+par_dir = os.path.abspath(os.path.join(f, os.pardir))           # parent dir
+gp_dir = os.path.abspath(os.path.join(par_dir, os.pardir))       # grand-parent dir
+sys.path.append(par_dir)
+sys.path.append(gp_dir)
+
+import utilities.args as au
 
 """
 Engset Model: FCFS M/M/m/K/N
@@ -13,19 +24,22 @@ Model parameters
   mu(k) = k * mu if 0<= k <= m
   mu(k) = m * mu if k > m
   r = lambda / mu
-  p(k) = prob k in the multiserver queue
+  p(k) = prob k in the multiserver queue (waiting + in svc)
 
 B(n, k): binomial coefficient
 B(n, k) = B(n-1, k-1) + B(n-1, k) with B(n, 0) = 1 and B(0, k) = 0
 p(k) = p(0) * r^k * B(N, k)                            0 <= k <= m
 p(k) = p(0) * r^k * B(N, k) * (k!/m!) * m^(m-k)        m <= k <= K
-     = p(m) * (N-m)! / (N- k)! * r^(k - m) * m^(m-k)   m <= k <= K
-
-p(k, N) = f(k, N) / Sum_{k=0}^N f(k, N)
+     = p(m) * (N-m)! / (N - k)! * r^(k - m) * m^(m-k)   m <= k <= K
 
 Recursions
 f(0, N) = 1, N >= 0
 f(k, N) = f(k, N-1) * N / (N - k)          0 <= k < N, N >= 1
+p(k, N) = f(k, N) / Sum_{k=0}^N f(k, N)
+
+p(k, N) = p(k-1, N) * r * (N+1-k) / k                0 < k <= m
+p(k, N) = p(k-1, N) * r * (N+1-k) / m                    m < k
+p(k, N) = p(k-1, N) * r * (N+1-k) / min(k, m)         0 < k <= N
 
 Not implemented recursions
 f(0, N) = 1, N >= 0
@@ -80,42 +94,55 @@ class MachineRepair:
         self.mu = mu
         self.lmbda = lmbda
         self.servers = servers
-        self.jobs = np.float(jobs)
-        r = lmbda / np.float(mu)
-
-        self.avg_queue, self.avg_waiting, self.avg_insvc, self.var_queue = 0.0, 0.0, 0.0, 0.0
-        f = []
-        f[0], sum = 1.0, 1.0  / self.jobs    # divide sum by # jobs to improve converge at large values
-        for k in range(1, int(self.jobs) + 1):
-            min_k = np.float(k) if k <= self.servers else self.servers
-            f.append(f[- 1] * r * (self.jobs - (k - 1)) / min_k)
-            sum += f[-1] / self.jobs                               # divide by # jobs to improve converge at large values
-            if np.isnan(sum):
-                print 'overflow'
-                sys.exit(0)
-
-        self.probs = []
-        for k in range(len(f)):
-            pk = (f[k] / sum) / self.jobs
-            self.probs[k] = pk
-            self.avg_queue += k * pk
-            self.mr_kpis(k, pk)         # self.avg_waiting, self.avg_insvc
-
-        for k in range(len(f)):        # queue variance
-            self.var_queue += ((k - self.avg_queue) * (k - self.avg_queue)) * self.probs[k]
+        self.r = lmbda / np.float(mu)
+        self.jobs = jobs
+        if self.jobs == 0:
+            print 'no jobs'
+            sys.exit(0)
+        self.probs = np.zeros(1 + jobs, dtype=np.float)
+        self.probs[0] = 1.0
+        _ = [self.get_prob(k) for k in range(1, 1 + self.jobs)]
+        self.probs /= np.sum(self.probs)
+        self.avg_queue = np.average(np.arange(0, 1 + self.jobs), weights=self.probs)
 
         # more MR kpis
-        self.avg_thinking = self.jobs - self.avg_queue
-        self.tput = self.lmbda * (self.jobs - self.avg_queue)          # throughput (jobs/sec)
-        self.cycle_time = self.jobs / self.tput                     # time for a job to complete a cycle (think time + queueing time + svc time)
-        self.util = self.avg_insvc / self.servers                   # server utilization
+        self.avg_waiting, self.avg_in_svc, self.var_queue = 0.0, 0.0, 0.0
+        _ = [self.kpis(k) for k in range(1 + self.jobs)]
+        self.avg_thinking = np.float(self.jobs) - self.avg_queue
+        self.tput = self.lmbda * self.avg_thinking                  # throughput (jobs/sec)
+        self.cycle_time = np.float(self.jobs) / self.tput                     # time for a job to complete a cycle (think time + queueing time + svc time)
+        self.util = self.avg_in_svc / self.servers                   # server utilization
         self.prob0 = self.probs[0]                              # empty repair station
-        self.prob_waiting = self.avg_waiting / self.jobs
-        self.prob_insvc = self.avg_insvc / self.jobs
-        self.prob_thinking = self.avg_thinking / self.jobs
+        self.prob_waiting = self.avg_waiting / np.float(self.jobs)
+        self.prob_in_svc = self.avg_in_svc / np.float(self.jobs)
+        self.prob_thinking = self.avg_thinking / np.float(self.jobs)
 
-    def mr_kpis(self, k, pk):
-        w = k -self.servers if k > self.servers else 0   # number waiting
-        self.avg_waiting += w * pk                       # avg jobs waiting at repair station
-        bz = k if k <= self.servers else self.servers        # number of busy servers
-        self.avg_insvc += bz * pk                        # avg jobs being processed by servers
+    def get_prob(self, k):
+        self.probs[k] = self.probs[k-1] * self.r * (np.float(self.jobs) + 1.0 - k) / np.minimum(k, self.servers)
+
+    def kpis(self, k):
+        pk = self.probs[k]
+        w = k - self.servers if k > self.servers else 0   # number waiting
+        self.avg_waiting += w * pk                        # avg jobs waiting at repair station
+        bz = k if k <= self.servers else self.servers     # number of busy servers
+        self.avg_in_svc += bz * pk                         # avg jobs being processed by servers
+
+if __name__ == "__main__":
+    arg_dict = au.get_pars(sys.argv[1:])
+    servers, think_time, processing_time = arg_dict['servers'], arg_dict['think_time'], arg_dict['processing_time']
+    s_out = 'jobs, servers, r, queue, pwr, tput\n'
+    for s in servers:
+        for p in processing_time:
+            for jobs in range(1, 4 * s + 1):
+                    MR = MachineRepair(1.0 / think_time, 1.0 / p, s, jobs)
+                    d = {'servers': s, 'r': MR.r}
+                    tput = (jobs - MR.avg_queue) / np.float(think_time)
+                    pwr = MR.r * (float(jobs) / MR.avg_queue - 1.0)
+                    d['jobs'] = jobs
+                    d['queue'] = MR.avg_queue
+                    d['pwr'] = pwr
+                    d['tput'] = tput
+                    s_out += json.dumps(d) + '\n'
+    with open('/tmp/engset.json', 'w') as f:
+        f.write(s_out)
+
--- shared/hyperbolic.py
+++ shared/hyperbolic.py
@@ -1,3 +1,59 @@
 """
 hyperbolic approximation to machine repair model
+a = m^2 (1+2r) /(r^2(1+r)^2)
+n0 = mr/(1+r)
+q0 = m (r^2-r-1) / (r (1+r))
+q(N)=q0+sqrt(A + (N-n0)^2) for N>m
+q(N)=N r/(1+r) for N<=m
+
 """
+
+import numpy as np
+import sys
+import os
+import json
+
+# add the path to utilities
+f = os.path.dirname(os.path.realpath(__file__))
+par_dir = os.path.abspath(os.path.join(f, os.pardir))           # parent dir
+gp_dir = os.path.abspath(os.path.join(par_dir, os.pardir))       # grand-parent dir
+sys.path.append(par_dir)
+sys.path.append(gp_dir)
+
+import utilities.args as au
+
+
+class MR_hyperbolic:
+    def __init__(self, lmbda, mu, servers, jobs):
+        """
+        :param r: r parameter = service time / think time (lambda/mu)
+        :param m: servers
+        """
+        self.servers = servers
+        self.r = lmbda / np.float(mu)
+        self.a = servers**2 * (1.0 + 2.0 * self.r) / (self.r**2 * (1.0 + self.r)**2)
+        self.n0 = servers * self.r/(1.0 + self.r)
+        self.q0 = servers * (self.r**2 - self.r - 1.0) / (self.r * (1.0 + self.r))
+        self.avg_queue = self.q0 + np.sqrt(self.a + (jobs - self.n0)**2) if jobs > self.servers else jobs * self.r / (1.0 + self.r)
+
+
+if __name__ == '__main__':
+    arg_dict = au.get_pars(sys.argv[1:])
+    servers, think_time, processing_time = arg_dict['servers'], arg_dict['think_time'], arg_dict['processing_time']
+    s_out = ''
+    for s in servers:
+        for p in processing_time:
+            for jobs in range(1, 4 * s + 1):
+                MR = MR_hyperbolic(1.0 / think_time, 1.0 / p, s, jobs)
+                d = {'servers': s, 'r': MR.r}
+                tput = (jobs - MR.avg_queue) / np.float(think_time)
+                pwr = MR.r * (float(jobs) / MR.avg_queue - 1.0)
+                d['jobs'] = jobs
+                d['queue'] = MR.avg_queue
+                d['pwr'] = pwr
+                d['tput'] = tput
+                s_out += json.dumps(d) + '\n'
+    with open('/tmp/hyperbolic.json', 'w') as f:
+        f.write(s_out)
+
+
